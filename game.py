516d8a2057cf961bf33aa9f39795d457261bc92a
Dahong Luo <dappon.04@gmail.com>
2024-01-08 18:01:02 +0900

    added window rendering

--- game.py
+++ game.py
@@ -1,21 +1,27 @@
 import pygame
+import time
+#from ghosts import Blinky, Pinky, Inky, Clyde
+#from pacman import Pacman
+
+
+
 
 BOARD =[
  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
  [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
  [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
- [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+ [1, 0, 0, 0, 0, 3, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 3, 0, 0, 0, 0, 1],
  [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
  [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
  [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1],
  [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1],
  [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1],
  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
- [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
- [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
- [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
- [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+ [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 2, 2, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+ [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+ [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+ [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
  [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
  [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
  [1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1],
@@ -23,30 +29,66 @@ BOARD =[
  [1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1],
  [0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0],
  [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
- [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+ [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
  [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
  [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1],
  [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1],
- [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+ [1, 0, 1, 1, 0, 3, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 3, 0, 1, 1, 0, 1],
  [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
  [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
+
+#1: wall
+#0: coockie
+#2: ghost spawn
+#3: power up
+
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
-BLOCK_SIZE = 30
+BLOCK_SIZE = 20
+
+GAME_SPEED = 60
 
 
 class PacmanGame:
     def __init__(self, winndows_width=WINDOW_WIDTH, window_height=WINDOW_HEIGHT):
-        self.board = []
+        self.board = BOARD
         self.width = winndows_width
         self.height = window_height
-        self.blinky_pos
-        self.pinky_pos
-        self.inky_pos
-        self.clyde_pos
+        self.score = 0
+        self.clock = pygame.time.Clock()
+        """self.blinky = Blinky()
+        self.pinky = Pinky()
+        self.inky = Inky()
+        self.clyde = Clyde()
+        self.pacman = Pacman()
+        
+        self.ghosts = [self.blinky, self.pinky, self.inky, self.clyde]"""
+        
+        
+        
+    
+    def reset(self):
+        self.score = 0
+        
+        pass
+    
+    def update_window(self, window, cell_size):
+            for row in range(len(self.board)):
+                for col in range(len(self.board[row])):
+                    if self.board[row][col] == 1:
+                        pygame.draw.rect(window, (0, 0, 255), (col * cell_size, row * cell_size, cell_size, cell_size))
+                    elif self.board[row][col] == 0:
+                        pygame.draw.circle(window, (255, 255, 0), (col * cell_size + cell_size // 2, row * cell_size + cell_size // 2), cell_size // 10)
+                    elif self.board[row][col] == 2:
+                        pygame.draw.rect(window, (255, 0, 0), (col * cell_size, row * cell_size, cell_size, cell_size))
+                    elif self.board[row][col] == 3:
+                        pygame.draw.circle(window, (255, 0, 255), (col * cell_size + cell_size // 2, row * cell_size + cell_size // 2), cell_size // 5)
+
+            pygame.display.update()
+        
 
     def setup_board(self):
         # Set up the game window
@@ -55,11 +97,7 @@ class PacmanGame:
         window = pygame.display.set_mode((window_width, window_height))
         pygame.display.set_caption("Pacman Game")
 
-        # Set up the game board
-        board_width = 10
-        board_height = 10
-        cell_size = 50
-        self.board = [[0] * board_width for _ in range(board_height)]
+        cell_size = BLOCK_SIZE
 
         # Add code here to customize the game board
         # You can use Pygame's drawing functions to create the board layout
@@ -72,15 +110,26 @@ class PacmanGame:
                     running = False
 
             # Add code here to update and render the game board
-
-            pygame.display.update()
+            
+            """self.pacman.move()
+            self.pacman.update()
+            
+            for ghost in self.ghosts:
+                ghost.move(self.board, self.pacman)
+                """
+            
+            self.update_window(window, cell_size)
+            
+            self.clock.tick(GAME_SPEED)
 
         # Quit Pygame
+        
         pygame.quit()
 
 
 # Create an instance of the PacmanGame class
 
-pygame.init()
-game = PacmanGame()
-game.setup_board()
+if __name__ == "__main__":
+    pygame.init()
+    game = PacmanGame()
+    game.setup_board()
---
+++ ghosts.py
@@ -0,0 +1,12 @@
+class Ghost:
+    def __init__(self) -> None:
+        self.x
+        self.y
+    
+    def find_path(self, board, target):
+        pass
+
+class Pinky(Ghost):
+    def __init__(self) -> None:
+        pass
+    
